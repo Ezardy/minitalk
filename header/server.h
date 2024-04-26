980f37e48f84c12017298e90aee837e81719bc95
zanikin <zaharanikinmai@gmail.com>
2024-04-26 18:58:10 +0400

    server handler

--- header/server.h
+++ header/server.h
@@ -6,24 +6,26 @@
 /*   By: zanikin <zanikin@student.42yerevan.am>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 14:47:02 by zanikin           #+#    #+#             */
-/*   Updated: 2024/04/26 15:38:54 by zanikin          ###   ########.fr       */
+/*   Updated: 2024/04/26 18:45:29 by zanikin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
+# define TIMEOUT 1u
+
 # include <unistd.h>
 # include <sys/types.h>
 # include <signal.h>
 # include "libft.h"
+# include "ft_printf.h"
 
 typedef struct s_server
 {
-	int		running;
-	int		bits_received;
-	int		timeouted;
-	char	byte;
-	pid_t	pid;
+	int				bits_received;
+	unsigned int	sec;
+	char			byte;
+	pid_t			pid;
 }	t_server;
 #endif
--- source/server/main.c
+++ source/server/main.c
@@ -6,17 +6,47 @@
 /*   By: zanikin <zanikin@student.42yerevan.am>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 21:12:33 by zanikin           #+#    #+#             */
-/*   Updated: 2024/04/26 15:57:13 by zanikin          ###   ########.fr       */
+/*   Updated: 2024/04/26 18:47:11 by zanikin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
+static t_server	g_servd = {0, 0, 0, -1};
+
+static void	handler(int signum, siginfo_t *info, void *context);
+
 int	main(int argc, char **argv)
 {
-	struct sigaction	ka;
 	struct sigaction	ra;
 
-	sigemptyset(&ka.sa_mask);
 	sigemptyset(&ra.sa_mask);
+	ra.sa_flags = SA_RESTART | SA_SIGINFO;
+	ra.sa_handler = handler;
+	sigaction(SIGUSR1, &ra, NULL);
+	while (1)
+		g_servd.sec = sleep(TIMEOUT);
+}
+
+static void	handler(int signum, siginfo_t *info, void *context)
+{
+	(void)context;
+	if (g_servd.pid < 0)
+		g_servd.pid = info->si_pid;
+	if (g_servd.pid == info->si_pid)
+	{
+		g_servd.byte << 1;
+		if (signum == SIGUSR2)
+			g_servd.byte |= 1;
+		if (g_servd.bits_received < 8)
+			g_servd.bits_received++;
+		else
+		{
+			g_servd.bits_received = 0;
+			if (g_servd.byte)
+				ft_putchar_fd(g_servd.byte, 1);
+			else
+				g_servd.pid = -1;
+		}
+	}
 }
